68d5284f13eae9caa9e67c1a2c870d59a6da2e80
CPorterW <cporterwilliams03@gmail.com>
2024-06-20 19:54:23 -0700

    Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 4.0),
+            new Rectangle("Blue", 3.0, 5.0),
+            new Circle("Green", 2.5)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.Color}, Area: {shape.GetArea()}");
+        }
     }
 }
